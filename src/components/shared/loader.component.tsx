e990e525e2ff49f643ec154940e6b0fb481e72dd
Alexandr Kochegarov <35864804+HullPerse@users.noreply.github.com>
2026-05-01 22:03:17 +0300

    initial commit

---
+++ src/components/shared/loader.component.tsx
@@ -0,0 +1,33 @@
+import { cn } from "@/lib/utils";
+import { Box } from "lucide-react";
+
+export function BigLoader() {
+  return (
+    <main className="absolute flex h-screen w-screen flex-col items-center justify-center bg-background font-extrabold text-text">
+      <Box className="size-28 animate-spin" />
+    </main>
+  );
+}
+
+export function WindowLoader({ className }: { className?: string }) {
+  return (
+    <main
+      className={cn(
+        "flex h-full w-full flex-col items-center justify-center bg-card font-extrabold text-text",
+        className,
+      )}
+    >
+      <Box className="size-28 animate-spin" />
+    </main>
+  );
+}
+
+export function SmallLoader({
+  size,
+  className,
+}: {
+  size?: number;
+  className?: string;
+}) {
+  return <Box className={cn(`size- animate-spin ${size || 4}`, className)} />;
+}
